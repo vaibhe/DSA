e6fbb3ce2e0f4b9b7faf85bdf318e748189734e6
Vaibhe <vaibhavambati28@gmail.com>
2023-03-23 18:48:43 +0530

    Not Bad

---
+++ Arrays/231_Power_of _Two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+      int i = 0;
+      while ( i != 100){
+         if ( pow(2,i) == n){
+          return true;
+       }
+       i++;
+      }
+  return false;
+        }
+};
