3759bbb611d5b3730a00c1c82d4bd28ad0f778ff
Vaibhe <vaibhavambati28@gmail.com>
2023-03-25 19:19:42 +0530

    file added

---
+++ Others/EvenOdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num ;
+    cout << " Enter the number " << endl;
+    cin >> num;
+    if ( num % 2 == 0){
+        cout << " Its an even number " << endl;
+    }
+    else{
+        cout << " Its an Odd number " << endl;
+    }
+    return 0;
+    
+
+}
